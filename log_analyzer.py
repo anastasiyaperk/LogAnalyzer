46ee83b48b21fe792dac24fcddc1a8642cf28cfd
Perkova Anastasiya <44846835+anastasiyaperk@users.noreply.github.com>
2022-09-20 23:44:37 +0500

    Add base parser logic

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,8 +1,15 @@
 #  !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import argparse
+from collections import namedtuple
+from datetime import datetime
+import gzip
 import json
 import logging
+import os
+import re
+from statistics import median
+from typing import Generator, List, NamedTuple
 
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
@@ -10,32 +17,110 @@ import logging
 #                     '$request_time';
 
 config = {
-    "REPORT_SIZE": 1000,
+    "REPORT_SIZE": 1,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log",
-    "LOG_FILE": "log_analyzer.log"
+    "LOG_FILE": "log_analyzer.log",
+    "ERROR_THRESHOLD": 0.7,
     }
 
 # logging settings
-logfile_path = config["LOG_FILE"]
+logging.basicConfig(filename=config.get("LOG_FILE"),
+                    format='[%(asctime)s] %(levelname).1s %(message)s',
+                    datefmt='%Y.%m.%d %H:%M:%S',
+                    level=logging.INFO
+                    )
 logger = logging.getLogger()
-logging_level = logging.INFO
-logger.setLevel(logging_level)
-formatter = logging.Formatter(fmt='[%(asctime)s] %(levelname).1s %(message)s',
-                              datefmt='%Y.%m.%d %H:%M:%S'
-                              )
-if logfile_path:
-    log_handler = logging.FileHandler(logfile_path)
-else:
-    log_handler = logging.StreamHandler()
-
-log_handler.setLevel(logging_level)
-log_handler.setFormatter(formatter)
-logger.addHandler(log_handler)
 
 
-def main():
-    pass
+def find_log_file(logs_dir_path: str) -> NamedTuple:
+    """
+    Find the latest log file by its name
+
+    :param logs_dir_path: path to logs directory
+    :return: namedtuple with the latest log file name, extension, date
+    """
+    files = os.listdir(logs_dir_path)
+    date_regexp = r"\d{8}"
+    logfile_regex = f"nginx-access-ui.log-{date_regexp}(.gz)?"
+
+    nginx_log_files = sorted([file for file in files if re.match(logfile_regex, file)], reverse=True)
+
+    latest_logfile = nginx_log_files[0]
+    date = datetime.strptime(re.search(date_regexp, latest_logfile).group(0), "%Y%m%d")
+    ext = ".gz" if latest_logfile.endswith(".gz") else None
+    LogFile = namedtuple("LogFile", ["file", "date", "ext"])
+
+    return LogFile(file=latest_logfile, date=date, ext=ext)
+
+
+def log_file_reader(log_file_path: str) -> Generator:
+    """
+    Read log file by line
+
+    :param log_file_path: path to reading log file
+    :return: generator by lines
+    """
+    if log_file_path.endswith(".gz"):
+        log = gzip.open(log_file_path, 'r')
+    else:
+        log = open(log_file_path, "r")
+
+    for line in log:
+        yield line
+
+    log.close()
+
+
+def log_parser(log_file_path: str) -> List[dict]:
+    """
+    Parse log file and collect url statistics by request time
+
+    :param log_file_path:
+    :return: list of dicts with statistics values
+    """
+    log_lines = log_file_reader(log_file_path)
+    url_req_times = {}
+    all_requests_time = lines_count = 0
+
+    for line in log_lines:
+        line_fields = line.split()
+        url = line_fields[6]
+        request_time = float(line_fields[-1])
+        all_requests_time += request_time
+        lines_count += 1
+        try:
+            url_req_times[url].append(request_time)
+        except KeyError:
+            url_req_times[url] = [request_time]
+
+    # TODO add condition of unread lines threshold
+    results = []
+    for url, req_times in url_req_times.items():
+        req_count = len(req_times)
+        req_time_sum = sum(req_times)
+        results.append(
+            {"count": req_count,
+             "count_perc": (req_count / lines_count) * 100,
+             "time_sum": req_time_sum,
+             "time_perc": (req_time_sum / all_requests_time) * 100,
+             "time_avg": req_time_sum / req_count,
+             "time_max": max(req_times),
+             "time_med": median(req_times)
+             }
+            )
+
+    return results
+
+
+def main(config_: dict):
+    file_name, date, ext = find_log_file(config_["LOG_DIR"])
+    logger.info(f"Latest log file is {file_name} (date: {date})")
+    # TODO: Change condition
+    if file_name in os.listdir(config_["REPORT_DIR"]):
+        return
+
+    log_parser(os.path.join(config_["LOG_DIR"], file_name))
 
 
 if __name__ == "__main__":
@@ -46,9 +131,11 @@ if __name__ == "__main__":
         # Load config from file
         args = parser.parse_args()
         if args.config:
-            config = json.load(args.config)
+            logger.info(f"Using config from file {args.config}")
+            with open(args.config, 'r') as f:
+                config.update(json.load(f))
 
-        # print(config)
+        main(config)
 
     except Exception as ex:
         logger.error(ex, exc_info=True)
